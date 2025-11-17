8b36f2de373090cd649fc88d4e65800b707bf204
alireza <theethanwardhere@gmail.com>
2025-11-17 21:29:59 +0330

    Version 1.0.0

--- src/main/java/com/arcade/slowrat/entity/User.java
+++ src/main/java/com/arcade/slowrat/entity/User.java
@@ -1,6 +1,10 @@
 package com.arcade.slowrat.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.Date;
@@ -16,14 +20,31 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 8, max = 50)
+    @NotBlank
     private String username;
+
     @Column(length = 60)
     private String password;
+
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 8, max = 210)
+    @NotBlank
+    @Email
     private String email;
+
     private String name;
+
     private String role;
+
     private String passwordHash;
+
     private Date createdAt;
+
     private boolean enabled = false;
 
 }
